c75569a0477df076742f13f9253a5b13268eb828
igalv <vilenskyigal@gmail.com>
2021-07-11 14:53:20 +0200

    youtube clone structure, icons, header component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBR7BIs2JHN9TxdmaL0l4RHmZvUzqoqt_M",
+  authDomain: "clone-4ce88.firebaseapp.com",
+  projectId: "clone-4ce88",
+  storageBucket: "clone-4ce88.appspot.com",
+  messagingSenderId: "414545303336",
+  appId: "1:414545303336:web:2d5b64817b2e7641a8a21d",
+  measurementId: "G-DDXL44H8DH",
+};
