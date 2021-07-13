6823d26913189cc315e796a7b095c2e3445774a1
igalv <vilenskyigal@gmail.com>
2021-07-13 11:42:01 +0200

    search page components and style

--- src/searchPage/SearchPage.jsx
+++ src/searchPage/SearchPage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./searchPage.scss";
 import TuneOutLinedIcon from "@material-ui/icons/TuneOutlined";
+import ChannelRow from "../channelRow/ChannelRow";
+import VideoRow from "../videoRow/VideoRow";
 export default function SearchPage() {
   return (
     <div className="searchPage">
@@ -9,6 +11,17 @@ export default function SearchPage() {
         <h2>FILTERS</h2>
       </div>
       <hr />
+      <ChannelRow
+        image="https://yt3.ggpht.com/yti/APfAmoH0fn0MXtqS9WtxWp-PXRdK-5-v131rBthlXr88IQ=s88-c-k-c0x00ffffff-no-rj-mo"
+        channel="Igal Vilensky"
+        verified
+        subs="154K"
+        noOfVideos={382}
+        description="🍀 Music for relaxation, meditation, study, reading, massage, spa or sleep. This music is perfect for fighting anxiety, stress or insomnia as it helps you relax and get rid of bad vibrations."
+      />
+
+      <hr />
+      <VideoRow />
     </div>
   );
 }
---
+++ src/videoRow/VideoRow.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./videoRow.scss";
+function VideoRow({
+  views,
+  subs,
+  description,
+  timestamp,
+  channel,
+  title,
+  image,
+}) {
+  return <div className="videoRow"></div>;
+}
+
+export default VideoRow;
